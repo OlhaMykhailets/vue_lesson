056198d660416d40281f96786c57f977ce30f151
Olha <32494251+OlhaMichailets@users.noreply.github.com>
2022-01-08 01:16:25 +0200

    create

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <counter-app title='счетчик' color='red'></counter-app>
+  <counter-app title='еще один' color='green'></counter-app>
+</template>
+
+<script>
+import CounterApp from '@/components/counter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CounterApp
+  }
+}
+</script>
+
+<style lang="scss">
+#counter {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/counter.vue
@@ -0,0 +1,66 @@
+<template>
+    <div id="counter">
+
+        <h2 v-bind:class="color">{{ title }}</h2>
+        <span>{{ counter }}</span>
+        <button @click = 'increment'> Increment </button>
+        <button @click = 'decrement'> Decrement </button>
+
+        <input type="text" v-model.number = "minCount" placeholder="MinValue">
+        <input type="text" v-model.number = "maxCount" placeholder="MaxValue">
+
+        <ul v-if = "counter">
+            <li v-for = "click in counter" :key = "click">Click</li>
+        </ul>
+     </div>
+</template>
+
+<script>
+    export default {
+     name: "CounterApp",
+     props: ['title', 'color'],
+                data () {
+                    return {
+                        counter: 0,
+
+                        minCount: '',
+                        maxCount: '',
+                    };
+                },
+                methods: {
+                    increment() {
+                        this.counter++
+
+                        if (this.counter > this.maxCount) {
+                            this.counter = this.maxCount
+                        }
+                    },
+                    decrement() {
+                        this.counter--
+
+                        if (this.counter < this.minCount) {
+                            this.counter = this.minCount
+                        }
+                    },
+                },
+                computed: {
+
+                }
+            }
+
+</script>
+
+<style lang="scss" scouped>
+.yellow {
+    color: yellow;
+}
+
+.red {
+    color: red;
+}
+
+.green {
+    color: green;
+}
+
+</style>
